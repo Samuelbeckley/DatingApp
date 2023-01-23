52b4f3dd940043c7544be879e78463a5fb622582
Samuelbeckley <46754895+Samuelbeckley@users.noreply.github.com>
2023-01-23 15:23:27 -0500

    feature(medium): add authentication and authorization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // GET /api/users
+    [Authorize]
 
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -16,7 +16,7 @@ namespace API.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
